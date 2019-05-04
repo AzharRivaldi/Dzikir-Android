70422b125ad26359426cc4cc09ef81e2bf044178
Azhar Rivaldi <46733833+AzharRivaldi@users.noreply.github.com>
2019-05-05 05:10:59 +0700

    Add files via upload

---
+++ DzikirApp/app/src/main/java/com/azhar/dzikir/MainActivity.java
@@ -0,0 +1,84 @@
+package com.azhar.dzikir;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView dzikir;
+    int zikir = 0;
+    Vibrator vibe;
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dzikir = (TextView) findViewById(R.id.dzikir);
+        vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    public void subhanallah(View view) {
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.tasbih);
+        mediaPlayer.start();
+        zikir = zikir + 1;
+        tampil(zikir);
+        vibe.vibrate(100);
+    }
+
+    public void alhamdulillah(View view) {
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.tahmid);
+        mediaPlayer.start();
+        zikir = zikir + 1;
+        tampil(zikir);
+        vibe.vibrate(100);
+    }
+
+    public void lailahaillallah(View view) {
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.tahlil);
+        mediaPlayer.start();
+        zikir = zikir + 1;
+        tampil(zikir);
+        vibe.vibrate(100);
+    }
+
+    public void allahuakbar(View view) {
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.takbir);
+        mediaPlayer.start();
+        zikir = zikir + 1;
+        tampil(zikir);
+        vibe.vibrate(100);
+    }
+
+    public void tampil(int z) {
+        dzikir.setText("" + z);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.reset:
+                mediaPlayer.stop();
+                zikir = 0;
+                tampil(zikir);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
